90eb1d50fcfca4647fbd6832819be9055235edf5
daniel klein <daniel.klein@c4-security.com>
2016-03-09 13:49:11 +0200

    Fixed following issues: fixes #3 - adding the --conf argument in Main for configuration path fixes #6 - making the messanger client configurable in node configuration fixes #7  - bug in node NodeConfiguration serialization

---
+++ Monitron.Node.Tests/TestClientMessanger.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Monitron.Common;
+
+namespace Monitron.Node.Tests
+{
+	public class TestClientMessanger : IMessengerClient
+	{
+		private readonly Account r_Account;
+
+		public TestClientMessanger(Account i_Account)
+		{
+			this.r_Account = i_Account;
+		}
+
+		public Identity Identity
+		{
+			get
+			{
+				return r_Account.Identity;
+			}
+		}
+			
+		#region IMessengerClient implementation
+
+		public event EventHandler<MessageArrivedEventArgs> MessageArrived;
+
+		public event EventHandler<BuddySignedInEventArgs> BuddySignedIn;
+
+		public event EventHandler<BuddySignedOutEventArgs> BuddySignedOut;
+
+		public event EventHandler<BuddyListChangedEventArgs> BuddyListChanged;
+
+		public void sendMessage(Identity i_Buddy, string i_Message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void AddBuddy(Identity i_Identity, params string[] i_Groups)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RemoveBuddy(Identity i_Identity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public System.Collections.Generic.IEnumerable<BuddyListItem> Buddies {
+			get {
+				throw new NotImplementedException();
+			}
+		}
+		#endregion
+	}
+}
+
