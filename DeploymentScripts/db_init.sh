586053188f3dcd90f1091612397f0eea3363271c
Saggi Mizrahi <ficoos@gmail.com>
2016-03-12 21:57:52 +0200

    Vagrant
    Signed-off-by: Saggi Mizrahi <ficoos@gmail.com>

---
+++ DeploymentScripts/db_init.sh
@@ -0,0 +1,10 @@
+#!/bin/env sh
+mongo monitron << EOF
+db.dropUser("monitron_mgmt")
+db.createUser({
+    "user": "monitron_mgmt",
+    "pwd": "$1",
+    "roles": [ "readWrite" ]
+})
+EOF
+mongo monitron -u monitron_mgmt -p "$1"
--- ProsodyExtension/deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/env sh
-ansible-playbook -i monitron_inventory -u root --vault-password-file ./vault_password deploy.yml
