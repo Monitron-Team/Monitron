11de70fa92a1fc6e4b1bc43bdd7e45264d5ef8f2
TzafiW <tzafiwaisblat@gmail.com>
2016-06-05 20:47:44 +0300

    attributes

---
+++ Monitron.AI/RemoteCommandAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Monitron.AI
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class RemoteCommandAttribute : Attribute
+    {
+        public string MethodName { get; set; }
+        public string Description { get; set; }
+    }
+}
