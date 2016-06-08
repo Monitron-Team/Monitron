30fb1c358b7f1d0ddb591c70193578a0fc139ef6
Maor <maor.sade@gmail.com>
2016-06-08 11:51:20 +0300

    changed the way we send parameters to methods (by state instead of param_i)

--- Monitron.AI/AIML.cs
+++ Monitron.AI/AIML.cs
@@ -42,13 +42,6 @@ namespace Monitron.AI
             List<object> parameters =  new List<object>();
             parameters.Add(i_Buddy);
             parameters.Add(new State(user.Predicates));
-            if (int.TryParse(paramCountStr, out paramCountInt))
-            {
-                for (int i = 1; i <= paramCountInt; i++)
-                {
-                    parameters.Add(user.Predicates.grabSetting("param_" + i.ToString()));
-                }
-            }
             string input = res.Output;
             Regex rgx = new Regex("{{(\\w+)}}");
             string result = rgx.Replace(input, i_X=> this.r_MethodCache[i_X.Groups[1].Value].Invoke(null, parameters.ToArray()).ToString());
@@ -70,12 +63,6 @@ namespace Monitron.AI
             return result;
         }
 
-        public void UpdateAiml(string i_fullNameSpace, List<Tuple<string, int>> i_methodsToAiml)
-        {
-            XmlDocument doc = this.CreateXml(i_methodsToAiml);
-            this.m_bot.loadAIMLFromXML(doc,i_fullNameSpace+ ".aiml");
-        }
-
         private void initializeMethodsCache(object i_Obj)
         {
             MethodInfo[] myArrayMethodInfo =
@@ -91,15 +78,7 @@ namespace Monitron.AI
                     r_MethodCache.Add(attr.MethodName, meth);
                 }
             }
-
-            //foreach (MethodInfo metod in myArrayMethodInfo)
-            //{
-            //    this.r_MethodCache.Add(metod.Name, metod);
-            //}
-
         }
-
-
 
         public void r_MessengerClient_MessageArrived(object i_Sender, MessageArrivedEventArgs i_EventArgs)
         {
@@ -113,11 +92,9 @@ namespace Monitron.AI
                 // Nothing to do
                 return;
             }
-
-
             try
             {
-                returnedValue = this.Request(i_EventArgs.Message, i_EventArgs.Buddy);// ExecuteCommand(i_EventArgs.Buddy, arguments);
+                returnedValue = this.Request(i_EventArgs.Message, i_EventArgs.Buddy);
             }
             catch (TargetInvocationException e)
             {
@@ -143,72 +120,6 @@ namespace Monitron.AI
             {
                 // Server is down. Nothing we can do about it.
             }
-        }
-
-
-
-
-        private XmlDocument CreateXml( List<Tuple<string, int>> i_methodsToAiml)
-        {
-            XmlDocument doc = new XmlDocument();
-            XmlNode docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
-            doc.AppendChild(docNode);
-            XmlNode aimlNode = doc.CreateElement("aiml");
-            doc.AppendChild(aimlNode);
-
-            foreach (Tuple<string, int> tuple in i_methodsToAiml)
-            {
-                XmlNode newcategory = doc.CreateElement("category");
-                XmlNode pattern = doc.CreateElement("pattern");
-                newcategory.AppendChild(pattern);
-                string textPatters = tuple.Item1;
-                for (int i = 0; i < tuple.Item2; i++)
-                {
-                    textPatters += " *";
-                }
-                doc.CreateTextNode(textPatters);
-                pattern.AppendChild(doc.CreateTextNode(textPatters));
-                XmlNode think = doc.CreateElement("think");
-                XmlNode template = doc.CreateElement("template");
-                newcategory.AppendChild(template);
-                XmlNode setTag = doc.CreateElement("set");
-                XmlAttribute att = doc.CreateAttribute("name");
-                att.Value = "method_name" ;
-                setTag.Attributes.Append(att);
-                setTag.AppendChild(doc.CreateTextNode(tuple.Item1));
-                think.AppendChild(setTag);
-
-                XmlNode setTagIsMethod = doc.CreateElement("set");
-                XmlAttribute attIsMethod = doc.CreateAttribute("name");
-                attIsMethod.Value = "is_method";
-                setTagIsMethod.Attributes.Append(attIsMethod);
-                setTagIsMethod.AppendChild(doc.CreateTextNode("True"));
-                think.AppendChild(setTagIsMethod);
-
-                XmlNode setTagMethodParam = doc.CreateElement("set");
-                XmlAttribute attIsMethodParam = doc.CreateAttribute("name");
-                attIsMethodParam.Value = "method_param";
-                setTagMethodParam.Attributes.Append(attIsMethodParam);
-
-                
-                for (int j = 1; j <= tuple.Item2; j++)
-                {
-                    if (j > 1) //if not first
-                        setTagMethodParam.AppendChild(doc.CreateTextNode("_")); //adds space between parameters
-                    XmlAttribute starIndex = doc.CreateAttribute("index");
-                    starIndex.Value = j.ToString();
-                    XmlElement starElement = doc.CreateElement("star");
-                    starElement.Attributes.Append(starIndex);
-                    setTagMethodParam.AppendChild(starElement);
-                }
-                 think.AppendChild(setTagMethodParam);
-
-
-                template.AppendChild(think);
-                aimlNode.AppendChild(newcategory);
-
-            }
-            return doc;
         }
     }
 }
